3bfbc0b1baa3f66409d5cdb2224a4817287611de
Juanita Franco <franco_juanita@javeriana.edu.co>
2024-11-19 10:24:21 -0500

    Cragando todo

---
+++ src/main/java/com/example/parcialfrancoback/ParcialfrancobackApplication.java
@@ -0,0 +1,13 @@
+package com.example.parcialfrancoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialfrancobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialfrancobackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/parcialfrancoback/ParcialfrancobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcialfrancoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialfrancobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
